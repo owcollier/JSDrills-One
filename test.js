6811ddf9e56920ad57b766567db99e3eaaa2845d
Owcollier <owcollier@gmail.com>
2017-10-30 17:02:02 -0400

    added new yearOfBirth function

--- test.js
+++ test.js
@@ -1,7 +1,12 @@
-function whoAmI(name, age) {
+function yearOfBirth(age) {
   let yearOfBirth = (2017 - age);
+  return (yearOfBirth);
+}
+
+function whoAmI(name, age) {
+  let yob = yearOfBirth(age);
   console.log('Hi, my name is Chris and I\'m 29 years old');
-  console.log("I was born in " + yearOfBirth);
+  console.log('I was born in ' + yob);
 }
 
 whoAmI('Wade',27);
