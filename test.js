8dd3280b3d74097a17ff501cc544331372db91ef
Owcollier <owcollier@gmail.com>
2017-10-31 18:16:59 -0400

    solved the problems!

--- test.js
+++ test.js
@@ -1,13 +1,28 @@
+'use strict';
+
 function yearOfBirth(age) {
   if (age < 0) {
-    throw new Error('Age cannot be negative');
+    throw 'exception';
   }
   else {
     return 2017 - age;
   }
 }
 
+function validate(name,age) {
+  if (!name && !age) {
+    console.error('Arguments are not valid');
+  }
+}
+
+function typeCheck(name,age) {
+  if (typeof(name) !== 'string' && typeof(age) !== 'number') {
+    
+}
+
 function whoAmI(name, age) {
+  validate(name, age)
+  typeCheck(name, age)
   let yob = yearOfBirth(age);
   console.log('Hi, my name is Chris and I\'m 29 years old');
   console.log('I was born in ' + yob);
